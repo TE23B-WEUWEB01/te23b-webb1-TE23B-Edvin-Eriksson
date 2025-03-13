85c4708e3f7b8fb4950acf2077cf955d04519714
Edvin Eriksson <edvin.eriksson@elev.ga.ntig.se>
2025-03-13 11:10:23 +0100

    javascript intro

---
+++ kapitel 8/intro/js/script.js
@@ -0,0 +1,16 @@
+
+// var tal1 = 17;
+// var tal2 = 20;
+// var summa = tal1 + tal2 
+// alert(tall + " + " tal2 + "=" summa);
+
+
+var förnamn = "Edvin"
+var efternamn = "Eriksson"
+var helanamnet = förnamn + " " + efternamn
+var hobby = "sova"
+
+
+
+var mening = (förnamn + "\Topclass i huset" + efternamn)
+alert(mening);
